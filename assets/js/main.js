18fa7cac85111b9b28e8fcdc7484bb2fc3645707
lucaruboni <lucaruboni@gmail.com>
2023-03-07 16:42:23 +0100

    [add] name prompt
    [add] surname prompt
    
    [add] color prompt

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+alert("ciao");
+
+// Chiedi all'utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
+
+/* NOME */
+
+
+const userName = prompt("come ti chiami?");
+console.log(userName);
+
+
+/* COGNOME */
+
+const userSurname = prompt("Qual è il tuo cognome?");
+console.log(userSurname);
+
+/* COLORE */
+
+const userFavColor = prompt("Qual è il tuo colore preferito?")
+console.log(userFavColor)
